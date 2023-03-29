a323713eddfd954c35beecf443a9a31d5c7963fd
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-03-29 14:02:36 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,13 +7,15 @@ import { ContactsList } from 'components/ContactsList/ContactsList';
 import { Filter } from 'components/Filter/Filter';
 import { Wrapper } from './App.styled';
 
-const parseContacts = JSON.parse(localStorage.getItem('userContacts'))===null
-? [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ] : JSON.parse(localStorage.getItem('userContacts'));
+const parseContacts =
+  JSON.parse(localStorage.getItem('userContacts')) === null
+    ? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    : JSON.parse(localStorage.getItem('userContacts'));
 
 export const App = () => {
   const [contacts, setContacts] = useState(parseContacts);
