f8b7d38c5e712eb857e377fcd6a5bf94b3d3e893
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-03-29 13:36:00 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,7 +10,17 @@ import { Wrapper } from './App.styled';
 const parseContacts = JSON.parse(localStorage.getItem('userContacts'));
 
 export const App = () => {
-  const [contacts, setContacts] = useState(parseContacts);
+  const [contacts, setContacts] = useState(() => {
+    console.log(parseContacts);
+    return parseContacts.length !== 0
+      ? parseContacts
+      : [
+          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ];
+  });
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
@@ -43,12 +53,6 @@ export const App = () => {
 
   const changeFilter = e => setFilter(e.target.value);
 
-  // const getVisibleContacts = () => {
-
-  //   return contacts.filter(contact =>
-  //     contact.name.toLowerCase().includes(normalizedContact)
-  //   );
-  // };
   const normalizedContact = filter.toLowerCase();
   const visibleContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(normalizedContact)
